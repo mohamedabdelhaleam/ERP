187357a414b42cfc39a95c1b60fe8090ed6fa334
Mohamed Abdelhaleam <mohamedabdelhaleam13@gmail.com>
2025-11-28 22:51:18 +0200

    Update footer copyright year to 2025 and change contact link text for business inquiries

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -4,7 +4,7 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="footer-copyright">
-                        <p><span>© 2024</span><a target="_blank" href="https://wa.me/201095512791?text=I need Create a website for my business">Halim</a>
+                        <p><span>© 2025</span><a target="_blank" href="https://wa.me/201095512791?text=I need Your ERP System">Halim</a>
                         </p>
                     </div>
                 </div>
